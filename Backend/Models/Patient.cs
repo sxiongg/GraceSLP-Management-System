95206e1102ba0d95500febed58af54bfe276e7a9
Kelsey Matthews <kelsey@Kelseys-MBP.attlocal.net>
2018-04-23 09:57:10 -0700

    merge

--- Backend/Models/Patient.cs
+++ Backend/Models/Patient.cs
@@ -18,7 +18,6 @@ namespace GraceSLP
         public string Address { get; set; }
 
         public DateTime DOB { get;set; }
-<<<<<<< HEAD
 
         public List<DoctorNotes> DoctorNotes { get;set; }
 
@@ -32,15 +31,6 @@ namespace GraceSLP
 
         [ForeignKey( "LocationId" ) ]
 
-=======
-        public int DoctorId { get; set; }
-        public int LocationId { get; set; }
-
-        [ForeignKey( "DoctorId" ) ]
-        public Doctor Doctor { get; set; }
-        
-        [ForeignKey( "LocationId" ) ]
->>>>>>> 3058e273af985c0a293e659f3056fbeee5ea94ef
         public Location Location { get; set; }
 
         public List <Appointment> Appointments { get; set; } 
