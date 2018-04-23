5d19f1eb39953cba3c6b251a0f5214b5a0a1fb60
Sia X <sia.xiong@outlook.com>
2018-04-23 10:03:47 -0700

    none

--- Backend/Models/DoctorNotes.cs
+++ Backend/Models/DoctorNotes.cs
@@ -9,13 +9,7 @@ namespace GraceSLP
     public class DoctorNotes
     {
         public int Id { get; set; }
-<<<<<<< HEAD
-
         public string Note { get; set; }
-
-=======
-        public string Note { get; set; }
->>>>>>> 3058e273af985c0a293e659f3056fbeee5ea94ef
         public DateTime DateEntered { get; set; }
 
         public int PatientId { get; set; }
@@ -23,11 +17,5 @@ namespace GraceSLP
         [ForeignKey( "PatientId" ) ]
 
         public Patient Patient { get; set; }
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 3058e273af985c0a293e659f3056fbeee5ea94ef
     }
 }
--- Backend/Models/Patient.cs
+++ Backend/Models/Patient.cs
@@ -18,21 +18,6 @@ namespace GraceSLP
         public string Address { get; set; }
 
         public DateTime DOB { get;set; }
-<<<<<<< HEAD
-
-        public List<DoctorNotes> DoctorNotes { get;set; }
-
-        public int DoctorId { get; set; }
-
-        public int LocationId { get; set; }
-
-        [ForeignKey( "DoctorId" ) ]
-
-        public Doctor Doctor { get; set; }
-
-        [ForeignKey( "LocationId" ) ]
-
-=======
         public int DoctorId { get; set; }
         public int LocationId { get; set; }
 
@@ -40,7 +25,6 @@ namespace GraceSLP
         public Doctor Doctor { get; set; }
         
         [ForeignKey( "LocationId" ) ]
->>>>>>> 3058e273af985c0a293e659f3056fbeee5ea94ef
         public Location Location { get; set; }
 
         public List <Appointment> Appointments { get; set; } 
