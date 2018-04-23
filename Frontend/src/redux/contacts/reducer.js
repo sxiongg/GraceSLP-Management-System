6879af3a2f39fe20f221a6aca675a847fd67ce86
Kelsey Matthews <kelsey@Kelseys-MBP.attlocal.net>
2018-04-23 12:16:47 -0700

    merge

--- Frontend/src/redux/contacts/reducer.js
+++ Frontend/src/redux/contacts/reducer.js
@@ -2,29 +2,29 @@ import { Map } from "immutable";
 import fakeData from "../../containers/Contacts/fakeData";
 import contactActions from "./actions";
 
-const contacts = new fakeData(10).getAll();
+const contacts = new fakeData(2).getAll();
 
 const initState = new Map({
   contacts,
-  seectedId: contacts[0].id,
+  selectedId: contacts[0].id,
   editView: false
 });
 
 export default function contactReducer(state = initState, action) {
   switch (action.type) {
     case contactActions.CHANGE_CONTACT:
-      return state.set("seectedId", action.id).set("editView", false);
+      return state.set("selectedId", action.id).set("editView", false);
     case contactActions.ADD_CONTACT:
       return state
         .set("contacts", action.contacts)
-        .set("seectedId", action.selectedId)
+        .set("selectedId", action.selectedId)
         .set("editView", true);
     case contactActions.EDIT_CONTACT:
       return state.set("contacts", action.contacts);
     case contactActions.DELETE__CONTACT:
       return state
         .set("contacts", action.contacts)
-        .set("seectedId", action.seectedId);
+        .set("selectedId", action.selectedId);
     case contactActions.EDIT_VIEW:
       return state.set("editView", action.view);
     default:
